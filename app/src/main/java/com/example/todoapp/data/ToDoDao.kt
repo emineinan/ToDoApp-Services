fbc8692565801e2040e8cfb63308bceec8577609
Emine İnan <emineinanx@gmail.com>
2021-12-27 16:43:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/data/ToDoDao.kt
@@ -0,0 +1,18 @@
+package com.example.todoapp.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.todoapp.data.model.ToDoData
+
+@Dao
+interface ToDoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<ToDoData>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertData(toDoData: ToDoData)
+
+    @Delete
+    suspend fun deleteData(toDoData: ToDoData)
+}
---
+++ app/src/main/java/com/example/todoapp/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.todoapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todoapp.data.ToDoDatabase
+import com.example.todoapp.data.model.ToDoData
+import com.example.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+    private val toDoDao = ToDoDatabase.getDatabase(application).toDoDao()
+    private val repository: ToDoRepository = ToDoRepository(toDoDao)
+
+    val getAllData: LiveData<List<ToDoData>> = repository.getAllData
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+
+    fun deleteData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteData(toDoData)
+        }
+    }
+}
