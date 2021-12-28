582e1b0a2875ebc2fb067bff517356b15691af3a
Emine İnan <emineinanx@gmail.com>
2021-12-28 11:46:45 +0300

    Add insert and delete operation

--- app/src/main/java/com/example/todoapp/view/ToDoFragment.kt
+++ app/src/main/java/com/example/todoapp/view/ToDoFragment.kt
@@ -1,20 +1,61 @@
 package com.example.todoapp.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todoapp.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoapp.adapter.ToDoListAdapter
+import com.example.todoapp.data.model.ToDoData
+import com.example.todoapp.databinding.FragmentToDoBinding
+import com.example.todoapp.viewmodel.ToDoViewModel
 
 class ToDoFragment : Fragment() {
+    private var _binding: FragmentToDoBinding? = null
+    private val binding get() = _binding!!
+
+    private val toDoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ToDoListAdapter by lazy { ToDoListAdapter(toDoViewModel) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do, container, false)
+        _binding = FragmentToDoBinding.inflate(inflater, container, false)
+
+        setAdapter()
+
+        toDoViewModel.getAllData.observe(viewLifecycleOwner, { data ->
+            adapter.setData(data)
+        })
+
+        binding.buttonAddTask.setOnClickListener {
+            addTaskToDatabase()
+        }
+
+        return binding.root
+    }
+
+    private fun addTaskToDatabase() {
+        val title = binding.editTextTitle.text.toString()
+        val description = binding.editTextDescription.text.toString()
+        val newTask = ToDoData(0, title, description)
+        toDoViewModel.insertData(newTask)
+
+        clearInputFields()
+    }
+
+    private fun clearInputFields() {
+        binding.editTextTitle.text.clear()
+        binding.editTextDescription.text.clear()
+    }
+
+    private fun setAdapter() {
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
