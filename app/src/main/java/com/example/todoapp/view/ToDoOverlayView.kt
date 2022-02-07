1fbfd3aa66b5c5113d0cef1b9fe63f84363c4e15
Emine İnan <emineinanx@gmail.com>
2022-02-07 11:09:08 +0300

    Delete deprecated variable

--- app/src/main/java/com/example/todoapp/view/ToDoOverlayView.kt
+++ app/src/main/java/com/example/todoapp/view/ToDoOverlayView.kt
@@ -3,15 +3,15 @@ package com.example.todoapp.view
 import android.content.Context
 import android.graphics.PixelFormat
 import android.os.Build
-import android.util.DisplayMetrics
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.WindowManager
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.todoapp.databinding.ToDoOverlayViewBinding
+import android.content.res.Resources
+import android.widget.Toast
 
-class ToDoOverlayView(context: Context) {
-
+class ToDoOverlayView(private val context: Context) {
     private val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     private val layoutInflater =
         context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -34,24 +34,17 @@ class ToDoOverlayView(context: Context) {
         PixelFormat.TRANSLUCENT
     )
 
-    private fun getCurrentDisplayMetrics(): DisplayMetrics {
-        val dm = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(dm)
-        return dm
-    }
-
     private fun calculateSizeAndPosition(
         params: WindowManager.LayoutParams,
         widthInDp: Int,
         heightInDp: Int
     ) {
-        val dm = getCurrentDisplayMetrics()
-        // We have to set gravity for which the calculated position is relative.
+        val displayMetrics = Resources.getSystem().displayMetrics
         params.gravity = Gravity.START or Gravity.TOP
-        params.width = (widthInDp * dm.density).toInt()
-        params.height = (heightInDp * dm.density).toInt()
-        params.x = (dm.widthPixels - params.width) / 2
-        params.y = (dm.heightPixels - params.height) / 2
+        params.width = (widthInDp * displayMetrics.density).toInt()
+        params.height = (heightInDp * displayMetrics.density).toInt()
+        params.x = (displayMetrics.widthPixels - params.width) / 2
+        params.y = (displayMetrics.heightPixels - params.height) / 2
     }
 
     private fun initWindowParams() {
@@ -64,6 +57,11 @@ class ToDoOverlayView(context: Context) {
 
     private fun initWindow(): ConstraintLayout {
         binding = ToDoOverlayViewBinding.inflate(layoutInflater)
+
+        binding.buttonAdd.setOnClickListener {
+            Toast.makeText(context, "Button is working", Toast.LENGTH_SHORT).show()
+        }
+
         return binding.root
     }
 
@@ -79,5 +77,4 @@ class ToDoOverlayView(context: Context) {
     fun closeOverlay() {
         windowManager.removeView(binding.root)
     }
-
 }
