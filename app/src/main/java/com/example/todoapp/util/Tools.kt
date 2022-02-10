638563fa4cbb3e6df0f4e24b2286e767d386d8ac
Emine İnan <emineinanx@gmail.com>
2022-02-10 14:01:57 +0300

    Hide keyboard

--- app/src/main/java/com/example/todoapp/util/Tools.kt
+++ app/src/main/java/com/example/todoapp/util/Tools.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.example.todoapp.PermissionActivity
@@ -42,4 +44,10 @@ fun Context.startPermissionActivity() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
     )
+}
+
+fun View.hideKeyboard() {
+    val inputManager =
+        context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputManager.hideSoftInputFromWindow(windowToken, 0)
 }
